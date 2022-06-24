51d1849121edd24f4042b8842981479d4e4b33de
NizarMrad <nizar.mrad.pro@gmail.com>
2022-06-24 12:10:46 +0200

    reshuffle list

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { useState, useRef } from 'react';
 import './App.css';
 
 function App() {
-  const dragItem = useRef()
-  const dragOverItem = useRef()
+  const dragItem = useRef();
+  const dragOverItem = useRef();
   const [list, setList] = useState([
     'React',
     'Javascript',
@@ -11,16 +11,26 @@ function App() {
     'Gatsby',
     'Ruby on Rails',
   ]);
-  const dragStart=(e,position)=>{
-    dragItem.current=position
-    console.log(e.target.innerHTML)
-  }
 
-  const dragEnter = (e, position)=> {
-    dragOverItem.current=position
-    console.log(e.target.innerHTML)
+  const dragStart = (e, position) => {
+    dragItem.current = position;
+    console.log(e.target.innerHTML);
+  };
 
-  }
+  const dragEnter = (e, position) => {
+    dragOverItem.current = position;
+    console.log(e.target.innerHTML);
+  };
+
+  const drop = (e) => {
+    const copyListItems = [...list];
+    const dragItemContent = copyListItems[dragItem.current];
+    copyListItems.splice(dragItem.current, 1);
+    copyListItems.splice(dragOverItem.current, 0, dragItemContent);
+    dragItem.current = null;
+    dragOverItem.current = null;
+    setList(copyListItems);
+  };
   return (
     <>
       {list &&
@@ -35,8 +45,10 @@ function App() {
               color: 'white',
               cursor: 'pointer',
             }}
-            onDragStart={(e)=>dragStart(e,index)}
-            onDragEnter={(e)=>dragEnter(e,index)}
+            onDragStart={(e) => dragStart(e, index)}
+            onDragEnter={(e) => dragEnter(e, index)}
+            onDragEnd={drop}
+            onDragOver={(e) => e.preventDefault()}
             key={index}
             draggable
           >
